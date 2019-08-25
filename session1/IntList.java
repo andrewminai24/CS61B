a9f67c2dce2f8f0cbbd170547620325f79004338
andrewminai24 <andrewminai24@gmail.com>
2019-08-24 18:59:33 -0700

    list

---
+++ session1/IntList.java
@@ -0,0 +1,15 @@
+ class IntList{
+     int first;
+     IntList rest;
+
+    public IntList(int f,IntList r){
+        first = f;
+        rest = r;
+    }
+
+    public static void main(String[] args){
+        IntList L = new IntList(15,null);
+        L = new IntList(10,L);
+        L = new IntList(5,L);
+      }
+}
