40d92253a02dce7424dd6699af28c6a793999020
andrewminai24 <andrewminai24@gmail.com>
2019-09-04 17:25:54 -0700

    Slist add

--- session1/project1/SList.java
+++ session1/project1/SList.java
@@ -19,3 +19,11 @@ public SList(int x){
     sentinel = new IntNode(87312,null);
     sentinel.next = new IntNode(x,null);
 }
+
+@Override 
+public void print(){
+    for(Node p = sentinel.next; p!= null; p = p.next){
+        System.out.print(p + " ");
+    }
+    System.out.print();
+}
