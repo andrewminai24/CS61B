d2bfc5cd4befd688e04abada2e447c6a4b61d950
andrewminai24 <andrewminai24@gmail.com>
2019-09-06 18:06:54 -0700

    big O

--- session1/BigO/Test2.java
+++ session1/BigO/Test2.java
@@ -6,7 +6,23 @@ public class Test2{
         
         for(int first : numbers) //O(n)
             for(int second : numbers ) //O(n)
-                System.out.println(first + " ," second);
+                System.out.println(first + "," second);
     
+        }
+}
+
+/*
+
+public class Test2{
+    public static void log(String[] args){
+       //O(n ^ 3)
+       for(int number : numbers) //O(n)
+            System.out.println(number);
+        
+        for(int first : numbers) //O(n)
+            for(int second : numbers ) //O(n)
+                System.out.println(first + "," second);
+                for(int third : numbers)
+                    System.out.println(number);
         }
 }
