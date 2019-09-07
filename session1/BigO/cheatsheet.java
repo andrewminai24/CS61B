bd40cf43cdbe06c23892745f2e901f51cd809767
andrewminai24 <andrewminai24@gmail.com>
2019-09-06 17:43:39 -0700

    big o notation

---
+++ session1/BigO/cheatsheet.java
@@ -0,0 +1,13 @@
+/*
+Big O sheet:
+
+O(1) CONSTANT NO LOOPS
+usually searching algorithms have log(n) if they are sorted(Binary search) (not hash maps)
+
+O(n) Linear - for loops and while loops
+
+O(N^2) Quadratic- every element in a collection needs to be compared to every other element two nest loops
+
+0(n!) Factorial- you are adding a loop for every element.
+
+i
